6c8ec1c99c3624f75c9b694066a8394595663693
ALPHISTUBE <rsalphi@gmail.com>
2024-11-10 15:51:43 +0600

    scene

---
+++ src/components/home/home.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <div className='w-[100%] h-[100%]'>
+      <Spline
+        scene="https://prod.spline.design/bOT6-XTo0ulrHzQf/scene.splinecode" 
+      />
+    </div>
+  );
+}
